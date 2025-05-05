1179511d7b6a20eecc857ade87a4cdf0a4f9fb15
Aditya <mudliaraditya@gmail.com>
2025-05-05 12:08:33 +0530

    added usersidebar structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,57 @@
-  import React from "react";
-  import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-  import Login from "./Component/Login";
-  import Dashboard from "./Pages/Admin/Dashboard";
-  import Layout from "./Component/Layout"; // <-- import layout
-  import Viewall from "./Pages/Admin/Viewall";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Component/Login";
+import Dashboard from "./Pages/Admin/Dashboard";
+import Layout from "./Component/Layout"; // <-- import layout
+import Viewall from "./Pages/Admin/Viewall";
+import Usersidebar from "./Component/Usersidebar";
+import Sidebar from "./Component/Sidebar";
+import User from "./Pages/User/User";
 
-  function App() {
-    return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/view-all" 
+function App() {
+  return (
+    // <Route path="/" element={<Layout/>}>
+    //   <Route path="/view-all" element={<Viewall/>}/>
+    //   <Route path="//dashboard" element={<Dashboard />}/>
+    //   <Route path="/view-all" element={<Dashboard />}/>
+    //   <Route path="/view-all" element={<Viewall/>}/>
+    //   <Route path="/user" element={<User/>}/>
+
+
+
+    // </Route>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/view-all"
           element={
             <Layout>
 
-          <Viewall/>
-          </Layout>
+              <Viewall />
+            </Layout>
 
-        }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <Layout>
-                <Dashboard />
-              </Layout>
-            }
-          />
-        </Routes>
-      </Router>
-    );
-  }
+          }
+        />
+        
+        <Route path="/user"
+        element={<User/>} 
+        />
 
-  export default App;
+
+        <Route
+          path="/dashboard"
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout>
+          }
+        />
+      </Routes>
+    </Router>
+
+  
+  );
+}
+
+export default App;
